c3fefd75058372ae12f341947f76a1c5b3c470fc
Cody Baermann <cbaermann@comcast.net>
2019-10-29 15:09:40 -0700

    SelectionSort

--- SortingAlgorithms/Program.cs
+++ SortingAlgorithms/Program.cs
@@ -7,7 +7,7 @@ namespace SortingAlgorithms
     {
         static void Main(string[] args)
         {
-            
+
         }
         public static void BubbleSort(int[] array)
         {
@@ -22,6 +22,22 @@ namespace SortingAlgorithms
                 }
             }
         }
+
+        public static void SelectionSort(int[] array)
+        {
+            for(int partIndex = array.Length-1; partIndex > 0; partIndex--)
+            {
+                int largestAt = 0;
+                for(int i = 1; i <= partIndex; i++ )
+                {
+                    if(array[i]>array[largestAt])
+                    {
+                        largestAt = i;
+                    }
+                }
+                Swap(array, largestAt, partIndex);
+            }
+        }
         private static void Swap(int [] array, int i, int j)
         {
             if(i == j)
@@ -32,5 +48,8 @@ namespace SortingAlgorithms
             array[i]=array[j];
             array[j]=temp;
         }
+
+
+
     }
 }
