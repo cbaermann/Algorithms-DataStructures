cdac53536ab6e9ae2e9e262d7baeeb150876ac19
Cody Baermann <cbaermann@comcast.net>
2019-11-04 14:52:00 -0800

    started SLL

---
+++ Lists/LinkedLists.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lists
+{
+    
+    public class Node
+    {
+        public int value{get;set;}
+        public Node next{get;set;}
+    }
+}
--- Lists/Program.cs
+++ Lists/Program.cs
@@ -8,9 +8,26 @@ namespace Lists
     {
         static void Main(string[] args)
         {
-            ListDemo.Run();
+            // ListDemo.Run();
+            Node first = new Node() {value = 5};
+            Node second = new Node() {value = 1};
 
+            first.next = second;
+
+            Node third = new Node() {value = 3};
+            second.next = third;
+
+            PrintOutLinkedList(first);
             Console.Read(); 
+        }
+
+        private static void PrintOutLinkedList(Node node)
+        {
+            while(node!= null)
+            {
+                System.Console.WriteLine(node.value);
+                node = node.next;
+            }
         }
     }
 }
