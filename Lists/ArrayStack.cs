8263fd6d706f0dd5469af1fd1ede49e41d4813b9
Cody Baermann <cbaermann@comcast.net>
2019-11-05 20:27:32 -0800

    added ArrayStack

---
+++ Lists/ArrayStack.cs
@@ -0,0 +1,50 @@
+using System;
+using System.Collections.Generic;
+
+namespace Lists
+{
+    public class ArrayStack<T>
+    {
+        private T[] _items;
+
+        public ArrayStack()
+        {
+            const int defaultCapacity = 4;
+            _items = new T[defaultCapacity];
+        }
+
+        public ArrayStack(int capacity)
+        {
+            _items = new T[capacity];
+        }
+
+        public T Peek()
+        {
+            return _items[Count -1];
+        }
+
+        public void Pop()
+        {
+            if(IsEmpty)
+                throw new InvalidOperationException();
+            _items[--Count] = default(T);
+        }
+
+        public void Push(T item)
+        {
+            if(_items.Length == Count)
+            {
+                T[] largerArray = new T[Count * 2];
+                Array.Copy(_items, largerArray, Count);
+
+                _items = largerArray;
+            }
+            _items[Count++] = item;
+            //equal to [items][count]=item;
+            //count++
+        }
+
+        public bool IsEmpty => Count == 0;
+        public int Count{get; private set;}
+    }
+}
