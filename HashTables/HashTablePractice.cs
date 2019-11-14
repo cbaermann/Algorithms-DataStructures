2c6e723374e6b2b4bf35a928ae54f231e71c9a42
Cody Baermann <cbaermann@comcast.net>
2019-11-14 06:25:21 -0800

    hashtable

---
+++ HashTables/HashTablePractice.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections.Generic;
+
+namespace HashTables
+{
+    public class PhoneNumber
+    {
+        public string AreaCode{get;set;}
+        public string Exchange{get;set;}
+        public string Number{get;set;}
+
+        public override bool Equals(object obj)
+        {
+            var number = obj as PhoneNumber;
+            if(number == null)
+                return false;
+            return string.Equals(AreaCode, number.AreaCode)
+                && string.Equals(Exchange, number.Exchange)
+                && string.Equals(Number, number.Number);
+        }
+
+        public static bool operator == (PhoneNumber left, PhoneNumber right)
+        {
+            if(object.ReferenceEquals(left,right))
+                return true;
+            if(object.ReferenceEquals(null, left))
+                return false;
+            return left.Equals(right);
+        }
+
+        public static bool operator!=(PhoneNumber left, PhoneNumber right)
+        {
+            return !(left==right);
+        }public override int GetHashCode()
+        {
+            unchecked
+            {
+                const int HashingBase = (int)2166136261;
+                const int HashingMultiplyer = 16777619;
+
+                int hash = HashingBase;
+                hash= (hash * HashingMultiplyer) ^ (!object.ReferenceEquals(null, AreaCode) ? AreaCode.GetHashCode() : 0);
+                hash= (hash * HashingMultiplyer) ^ (!object.ReferenceEquals(null, Exchange) ? Exchange.GetHashCode() : 0);
+                hash= (hash * HashingMultiplyer) ^ (!object.ReferenceEquals(null, Number) ? Number.GetHashCode() : 0);
+
+                return hash;
+
+            }
+        }
+
+    }
+
+    public class Person 
+    {
+        public string Name{get;set;}
+        public int Age{get;set;}
+        public int SSN{get;set;}
+    }
+}
---
+++ HashTables/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashTables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number1 = new PhoneNumber(){AreaCode = "141804", Exchange="27", Number = "90319334"};
+            var number2 = new PhoneNumber(){AreaCode = "141804", Exchange="27", Number = "90319334"};
+            var number3 = new PhoneNumber(){AreaCode = "141804", Exchange="27", Number = "90319334"};
+
+            System.Console.WriteLine(number1.GetHashCode());
+            System.Console.WriteLine(number2.GetHashCode());
+            System.Console.WriteLine(number1==number2);
+            System.Console.WriteLine(number1.Equals(number2));
+
+            var customers = new Dictionary<PhoneNumber, Person>();
+            customers.Add(number1, new Person());
+            customers.Add(number2, new Person());
+
+            System.Console.WriteLine("After adding phone numbers.");
+
+            var c = customers[number3];
+            Console.Read();
+
+        }
+    }
+}
