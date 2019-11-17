8cf08501903f478e1947919d3848b137ecb1c9e5
Cody Baermann <cbaermann@comcast.net>
2019-11-16 21:23:49 -0800

    BST implimentation

---
+++ Trees/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+//root node, one root node in tree. Has no Parents
+//elements w/out children are "leaf nodes".
+//link between nodes is an edge. 
+//tree can not have cyclic paths.
+//Log(n) for insertion, deletion, and retrieval
+
+
+namespace Trees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
